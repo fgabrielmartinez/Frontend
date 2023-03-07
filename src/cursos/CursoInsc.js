bcef49ad44e4f837b88e105f46a0e4bb870865e9
fgabrielmartinez <112987966+fgabrielmartinez@users.noreply.github.com>
2023-03-07 19:59:19 -0300

    front ok sin seguridad

--- src/cursos/CursoInsc.js
+++ src/cursos/CursoInsc.js
@@ -1,35 +1,87 @@
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import React from "react";
 import { toast } from "react-toastify";
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
 
-class CursoInsc extends React.Component {
+class InternalCursoInsc extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      alumnoToDelete: {},
-      modalConfirmarEliminacion: false,
+
+
       alumno: [],
-      persona: []
+      alumnos_curso: []
     };
     this.onDelete = this.onDelete.bind(this);
-    this.handleClose = this.handleClose.bind(this);
-    this.handleOpen = this.handleOpen.bind(this);
+    this.onAdd = this.onAdd.bind(this);
+    this.handleSave = this.handleSave.bind(this);
+
   }
 
-  handleClose() {
-    this.setState({
-      modalConfirmarEliminacion: false
-    });
+  handleSave() {
+    let request3 = {
+      method: 'POST',
+      body: JSON.stringify({
+        id_curso: this.props.params.id,
+        id_alumnos: this.state.alumnos_curso
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+        "Accept": 'application/json',
+        "authorization": sessionStorage.getItem('token')
+      }
+    };
+    let id_curso = 2
+    fetch(`http://localhost:8080/api/curso/insc/${this.props.params.id}`, request3)
+      .then(res => {
+        return res.json().then(body => {
+          return {
+            status: res.status,
+            ok: res.ok,
+            headers: res.headers,
+            body: body
+          };
+        });
+      })
+      .then(result => {
+        if (result.ok) {
+          toast.succes(result.body.message, {
+            position: "bottom-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+          });
+        } else {
+          toast.error(result.body.message, {
+            position: "bottom-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+          });
+        }
+      },
+        (error) => {
+          console.log(error);
+          this.setState({
+            error,
+            alumno: [],
+            modalConfirmarEliminacion: false
+          });
+        }
+      )
+
   }
-  handleOpen(alumno) {
-    this.setState({
-      alumnoToDelete: alumno,
-      modalConfirmarEliminacion: true
-    });
-  }
+
 
   onDelete(alumno) {
     let alumnos_curso = [...this.state.alumnos_curso];
@@ -47,9 +99,6 @@ class CursoInsc extends React.Component {
   };
 
   componentDidMount() {
-    //  let alumnos_curso = [4,5,6];
-
-    //  this.setState({ alumnos_curso: alumnos_curso })
     let request2 = {
       method: 'GET',
       headers: {
@@ -58,48 +107,48 @@ class CursoInsc extends React.Component {
         "authorization": sessionStorage.getItem('token')
       }
     };
-    let id_curso=2
-    fetch(`http://localhost:8080/api/curso/insc/`+id_curso, request2)
-    .then(res => {
-      return res.json().then(body => {
-        return {
-          status: res.status,
-          ok: res.ok,
-          headers: res.headers,
-          body: body
-        };
-      });
-    })
-    .then(result => {
-      if (result.ok) {
-        let alumnos_curso=[];
-       
-         for(var i=0; i<result.body.length; i++){
-           alumnos_curso.push(result.body[i].id_alumno)
-         }
-        this.setState({ alumnos_curso:alumnos_curso});
-      } else {
-        toast.error(result.body.message, {
-          position: "bottom-right",
-          autoClose: 5000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-          theme: "light",
+    let id_curso = 2
+    fetch(`http://localhost:8080/api/curso/insc/${this.props.params.id}`, request2)
+      .then(res => {
+        return res.json().then(body => {
+          return {
+            status: res.status,
+            ok: res.ok,
+            headers: res.headers,
+            body: body
+          };
         });
-      }
-    },
-      (error) => {
-        console.log(error);
-        this.setState({
-          error,
-          alumno: [],
-          modalConfirmarEliminacion: false
-        });
-      }
-    )
+      })
+      .then(result => {
+        if (result.ok) {
+          let alumnos_curso = [];
+
+          for (var i = 0; i < result.body.length; i++) {
+            alumnos_curso.push(result.body[i].id_alumno)
+          }
+          this.setState({ alumnos_curso: alumnos_curso });
+        } else {
+          toast.error(result.body.message, {
+            position: "bottom-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+          });
+        }
+      },
+        (error) => {
+          console.log(error);
+          this.setState({
+            error,
+            alumno: [],
+            modalConfirmarEliminacion: false
+          });
+        }
+      )
 
 
 
@@ -210,8 +259,8 @@ class CursoInsc extends React.Component {
         <div className="row">
           <div className="col">
             <abbr title="Guardar Inscripción">
-              <button className="btn btn-primary" onClick={() => this.onDelete()}>
-               <span>Guardar</span>
+              <button className="btn btn-primary" onClick={() => this.handleSave()}>
+                <span>Guardar</span>
               </button>
             </abbr>
           </div>
@@ -221,6 +270,11 @@ class CursoInsc extends React.Component {
     );
   }
 }
+export function CursoInsc(props) {
+  const navigate = useNavigate();
+  const params = useParams();
 
+  return <InternalCursoInsc navigate={navigate} params={params} />
+}
 
 export default CursoInsc;
